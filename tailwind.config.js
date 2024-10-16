66ca3c4c107bd18b2a92ca5a7897b55d875a16ab
Mark Fasel <markfasel@gmail.com>
2024-10-16 10:20:22 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
